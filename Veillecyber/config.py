60fa12765225e22e63590833038150c2dfcab5b6
raphael-attias <137683618+raphael-attias@users.noreply.github.com>
2025-04-17 08:39:22 +0200

    Update config.py

--- Veillecyber/config.py
+++ Veillecyber/config.py
@@ -1,8 +1,6 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()  # Charge le fichier .env
-
+# Les variables d'environnement sont directement accessibles sans besoin de charger un fichier .env
 DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
 MISTRAL_API_ENDPOINT = os.getenv("MISTRAL_API_ENDPOINT")
 MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
