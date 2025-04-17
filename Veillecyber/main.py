13d6f8d449d324cf450363b7214c4297106932ce
raphael-attias <137683618+raphael-attias@users.noreply.github.com>
2025-04-17 17:55:40 +0200

    fix main for App.log

--- Veillecyber/main.py
+++ Veillecyber/main.py
@@ -44,7 +44,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[
-        logging.FileHandler("app.log"),
+        logging.FileHandler("App.log"),
         logging.StreamHandler()
     ]
 )
