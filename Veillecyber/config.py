43eba3a2bc32bb50cb2ca036f0bf18b318f8f0d8
raphael-attias <raphael-attias@users.noreply.github.com>
2026-04-26 17:18:44 +0000

    deploy: fe71b5a2e123bbf9b177b6a1cc64fdf27eec9a12

--- Veillecyber/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-# Les variables d'environnement sont directement accessibles sans besoin de charger un fichier .env
-DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
-MISTRAL_API_ENDPOINT = os.getenv("MISTRAL_API_ENDPOINT")
-MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
